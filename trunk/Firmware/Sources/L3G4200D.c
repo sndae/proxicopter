de7d408bd3f8782390844f7d33ebfc9fbd911f42
proximus@gbg.bg <proximus@gbg.bg@a2f8ba9e-4b07-127c-d546-808a9cbdf683>
2012-11-16 13:48:01 +0000

    update

--- trunk/Firmware/Sources/L3G4200D.c
+++ trunk/Firmware/Sources/L3G4200D.c
@@ -14,10 +14,55 @@
 **
 ** ###################################################################*/
 
-#include "Quadro_Public.h"
+#include "L3G4200D.h"
 #include "I2C2.h"
 
+static float   gyroRate[3] = {0.0,0.0,0.0};
+static Int32   gyroZero[3] = {0,0,0};
+static UInt32  gyroSample[3] = {0,0,0};
+static float   gyroScaleFactor = 0.0;
+static float   gyroHeading = 0.0;
+static UInt32  gyroLastMesuredTime = 0;
+static byte    gyroSampleCount = 0;
+
 QuadRes L3G4200D_Init(void){
-  
+  UInt8 bChar = 0;
+  Uint8 bRes = ERR_OK;
+  I2C2_SelectSlave(L3G4200D_I2C_ADDR);
+  I2C2_SendChar(0xFF);
+  I2C2_RecvChar(&bChar);
+
   return S_OK;
 }
+
+QuadRes L3G4200D_MeasuregyroSum(){
+
+  return S_OK;
+}
+
+QuadRes L3G4200D_EvaluateGyroRate(){
+
+  return S_OK;
+}
+
+QuadRes L3G4200D_InitializeGyro(){
+
+  return S_OK;
+}
+
+QuadRes L3G4200D_MeasureGyro(){
+
+  return S_OK;
+}
+
+QuadRes L3G4200D_CalibrateGyro(){
+  r
+  eturn S_OK;
+}
+
+QuadRes L3G4200D_ReadGyroTemp(){
+
+  return S_OK;
+}
+
+
