a0232180c98acb9aef68f412579b8b903df02c28
proximus@gbg.bg <proximus@gbg.bg@a2f8ba9e-4b07-127c-d546-808a9cbdf683>
2013-02-23 17:10:50 +0000

    Update. Diagram visio files added

--- Phonebook/trunk/TSQL_Scripts/PhoneBook.sql
+++ Phonebook/trunk/TSQL_Scripts/PhoneBook.sql
@@ -10,32 +10,32 @@ CREATE  DATABASE phonebook
 USE phonebook
 /* create tables */
 CREATE TABLE cities(id      int primary key not null,
-					          rev_nmb	int,
-					          code    nvarchar(6)	 ,
-					          name    nvarchar(64) ,
-					          area    nvarchar(64))
+					          rev_nmb	int             not null,
+					          code    nvarchar(6)	not null ,
+					          name    nvarchar(64)not null ,
+					          area    nvarchar(64)not null)
 PRINT '"cities" table was created'					
 
 CREATE TABLE phones(id         int primary key not null,
-					          rev_nmb	   int,
-					          code       nvarchar(5) ,
-				            phone_type nvarchar(64) )
+					          rev_nmb	   int             not null,
+					          code       nvarchar(5) not null,
+				            phone_type nvarchar(64)not null )
 PRINT '"phones" table was created'					
 
 CREATE TABLE subscribers(id          int primary key not null,
-	  					           rev_nmb	   int,
-						             code        dec ,
+	  					           rev_nmb	   int not null,
+						             code        dec not null,
 						             first_name  nvarchar(64)  not null,
 						             second_name nvarchar(64)  not null,
 						             third_name  nvarchar(64)  not null,
-						             ident_nmb	 nvarchar(10)     not null,
+						             ident_nmb	 nvarchar(10)  not null,
 						             city_id	   int           not null, 
 						             city_addr	 nvarchar(128) not null, 
 						             foreign key (city_id) references cities (id))
 PRINT '"subscribers" table was created'
 
 CREATE TABLE subscriber_phone_numbers(id  int primary key    not null,
-									                    rev_nmb	    int,
+									                    rev_nmb	    int        not null,
 									                    subscriber_id int          not null,
 								 	                    phone_id	    int          not null,
 								 	                    phone_number  nvarchar(64) not null,
