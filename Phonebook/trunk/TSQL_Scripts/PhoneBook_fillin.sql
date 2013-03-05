c8796b6aa927f968f0d60cbb729fb0222c882338
proximus@gbg.bg <proximus@gbg.bg@a2f8ba9e-4b07-127c-d546-808a9cbdf683>
2013-03-05 17:00:44 +0000

    update

--- Phonebook/trunk/TSQL_Scripts/PhoneBook_fillin.sql
+++ Phonebook/trunk/TSQL_Scripts/PhoneBook_fillin.sql
@@ -1,106 +1,106 @@
 
-DECLARE @rev_nmbr int
+DECLARE @REV_NUMB int
 
-SET @rev_nmbr = '0'
+SET @REV_NUMB = '0'
 
-INSERT INTO [phonebook].[dbo].[cities]
-           ([id]
-           ,[rev_nmb]
-           ,[code]
-           ,[name]
-           ,[area])
+INSERT INTO [phonebook].[dbo].[CITIES]
+           ([ID]
+           ,[REV_NUMB]
+           ,[CODE]
+           ,[NAME]
+           ,[AREA])
      VALUES
            ('0'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,N'052'
            ,N'Варна'
            ,N'Варна')
 
-INSERT INTO [phonebook].[dbo].[cities]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,[name]
-           ,[area])
+INSERT INTO [phonebook].[dbo].[CITIES]
+           ([ID]
+           ,[REV_NUMB]
+           ,[CODE]
+           ,[NAME]
+           ,[AREA])
      VALUES
            ('1'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,N'056'
            ,N'Бургас'
            ,N'Бургас')
            
-INSERT INTO [phonebook].[dbo].[cities]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,[name]
-           ,[area])
+INSERT INTO [phonebook].[dbo].[CITIES]
+           ([ID]
+           ,[REV_NUMB]
+           ,[CODE]
+           ,[NAME]
+           ,[AREA])
      VALUES
            ('2'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,N'02'
            ,N'София'
            ,N'София')
 
-INSERT INTO [phonebook].[dbo].[cities]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,[name]
-           ,[area])
+INSERT INTO [phonebook].[dbo].[CITIES]
+           ([ID]
+           ,[REV_NUMB]
+           ,[CODE]
+           ,[NAME]
+           ,[AREA])
      VALUES
            ('3'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,N'032'
            ,N'Пловдив'
            ,N'Пловдив')                                  
            
-INSERT INTO [phonebook].[dbo].[phones]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,phone_type)
+INSERT INTO [phonebook].[dbo].[PHONE_TYPES]
+           ([ID]
+           ,[REV_NUMB]           
+           ,[CODE]
+           ,[PHONE_TYPE])
      VALUES
            ('0'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,'11'
            ,N'Мобилен')    
            
-INSERT INTO [phonebook].[dbo].[phones]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,phone_type)
+INSERT INTO [phonebook].[dbo].[PHONE_TYPES]
+           ([ID]
+           ,[REV_NUMB]           
+           ,[CODE]
+           ,[PHONE_TYPE])
      VALUES
            ('1'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,'22'
            ,N'Офис')
                                                
-INSERT INTO [phonebook].[dbo].[phones]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,phone_type)
+INSERT INTO [phonebook].[dbo].[PHONE_TYPES]
+           ([ID]
+           ,[REV_NUMB]           
+           ,[CODE]
+           ,[PHONE_TYPE])
      VALUES
            ('2'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,'33'
            ,N'Личен')
            
-INSERT INTO [phonebook].[dbo].[subscribers]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,[first_name]
-           ,[second_name]
-           ,[third_name]
-           ,[ident_nmb]
-           ,[city_id]
-           ,[city_addr])
+INSERT INTO [phonebook].[dbo].[SUBSCRIBERS]
+           ([ID]
+           ,[REV_NUMB]           
+           ,[CODE]
+           ,[FIRST_NAME]
+           ,[SECOND_NAME]
+           ,[THIRD_NAME]
+           ,[IDENT_NUMB]
+           ,[CITY_ID] 
+           ,[CITY_ADDR])
      VALUES
            ('0'
-           ,@rev_nmbr          
+           ,@REV_NUMB          
            ,'989'
            ,N'Димитър' 
            ,N'Костов'
@@ -109,19 +109,19 @@ INSERT INTO [phonebook].[dbo].[subscribers]
            ,'0'
            ,'Синчец 9')
 
-INSERT INTO [phonebook].[dbo].[subscribers]
-           ([id]
-           ,[rev_nmb]           
-           ,[code]
-           ,[first_name]
-           ,[second_name]
-           ,[third_name]
-           ,[ident_nmb]
-           ,[city_id]
-           ,[city_addr])
+INSERT INTO [phonebook].[dbo].[SUBSCRIBERS]
+           ([ID]
+           ,[REV_NUMB]           
+           ,[CODE]
+           ,[FIRST_NAME]
+           ,[SECOND_NAME]
+           ,[THIRD_NAME]
+           ,[IDENT_NUMB]
+           ,[CITY_ID] 
+           ,[CITY_ADDR])
      VALUES
            ('1'
-           ,@rev_nmbr          
+           ,@REV_NUMB          
            ,'1203'
            ,N'Иван' 
            ,N'Георгиев'
@@ -132,28 +132,28 @@ INSERT INTO [phonebook].[dbo].[subscribers]
 
            
            
-INSERT INTO [phonebook].[dbo].[subscriber_phone_numbers]
-           ([id]
-           ,[rev_nmb]           
-           ,[subscriber_id]
-           ,[phone_id]
-           ,[phone_number])
+INSERT INTO [phonebook].[dbo].[SUBSCRIBER_PHONE_NUMBERS] 
+           ([ID] 
+           ,[REV_NUMB]            
+           ,[SUBSCRIBER_ID] 
+           ,[PHONE_ID] 
+           ,[PHONE_NUMB] )
      VALUES
            ('0'
-           ,@rev_nmbr           
+           ,@REV_NUMB           
            ,'0'
            ,'2'
            ,N'785209')
 
-INSERT INTO [phonebook].[dbo].[subscriber_phone_numbers]
-           ([id]
-           ,[rev_nmb]           
-           ,[subscriber_id]
-           ,[phone_id]
-           ,[phone_number])
+INSERT INTO [phonebook].[dbo].[SUBSCRIBER_PHONE_NUMBERS] 
+           ([ID] 
+           ,[REV_NUMB]            
+           ,[SUBSCRIBER_ID] 
+           ,[PHONE_ID] 
+           ,[PHONE_NUMB] )
      VALUES
            ('1'
-           ,@rev_nmbr
+           ,@REV_NUMB
            ,'1'
            ,'0'
            ,N'0878123456')
